72c51f1925ff081485c53792bbc8104880ddf0e5
jaypatel0717 <jaypatel0717@gmail.com>
2020-08-28 16:23:07 -0400

    adding google clone project

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCBsA1VaWyIYG5EKTSsOMfeOe3IwgmO2lI",
+    authDomain: "clone-c7235.firebaseapp.com",
+    databaseURL: "https://clone-c7235.firebaseio.com",
+    projectId: "clone-c7235",
+    storageBucket: "clone-c7235.appspot.com",
+    messagingSenderId: "48058097458",
+    appId: "1:48058097458:web:f0022e4055d1bfa8d3012b",
+    measurementId: "G-LG45B2B34F"
+  };
