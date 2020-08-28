529c97db79b1b5c5b966f6b2fe354041f8d7b993
jaypatel0717 <jaypatel0717@gmail.com>
2020-08-28 17:38:48 -0400

    design home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Css/App.css';
+import Home from './Pages/Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-        <h2>Hello</h2>
+      <Router>
+        <Switch>
+
+          <Route path="/search">
+            <h2>Search</h2>
+          </Route>
+
+          <Route path="/">
+            <Home/>
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
